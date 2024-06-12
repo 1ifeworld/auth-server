dabdae77f6899dafacba470f2629ee86758617b3
jawndiego <jawndiego@gmail.com>
2024-06-12 13:56:50 -0400

    lucia int cont

--- index.ts
+++ index.ts
@@ -4,9 +4,57 @@ import { ed25519, ed25519ph} from "@noble/curves/ed25519"
 import { blake3 } from "@noble/hashes/blake3"
 import AWS from 'aws-sdk'
 import { TextEncoder } from 'util'
+import { csrf } from 'hono/csrf'
+import { getCookie } from "hono/cookie"
+import type { User, Session } from 'lucia'
+import { lucia } from "./sessions"
 
+import { verifyRequestOrigin } from "lucia"
+import { origin } from "bun"
 
-const app = new Hono()
+verifyRequestOrigin(origin, [ "https://www.river.ph/*"])
+
+const app = new Hono<{
+	Variables: {
+		user: User | null
+		session: Session | null
+	}
+}>()
+
+// cross site request forgery helper 
+app.use(csrf())
+
+app.use("*", async (c, next) => {
+	const sessionId = getCookie(c, lucia.sessionCookieName) ?? null
+	if (!sessionId) {
+		c.set("user", null)
+		c.set("session", null)
+		return next()
+	}
+	const { session, user } = await lucia.validateSession(sessionId)
+	if (session && session.fresh) {
+		// use `header()` instead of `setCookie()` to avoid TS errors
+		c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), {
+			append: true
+		})
+	}
+	if (!session) {
+		c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), {
+			append: true
+		})
+	}
+	c.set("user", user)
+	c.set("session", session)
+	return next()
+})
+
+app.get("/", async (c) => {
+	const user = c.get("user")
+	if (!user) {
+		return c.body(null, 401)
+	}
+
+})
 
 const { Client } = pg
 

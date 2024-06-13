242bd49f2840b649de13475113bb7163bb5d3b24
jawndiego <jawndiego@gmail.com>
2024-06-13 13:43:35 -0400

    hmm

--- index.ts
+++ index.ts
@@ -377,32 +377,32 @@ import { writeClient } from './watcher'
 // verifyRequestOrigin(origin, ["https://www.river.ph/*"])
 
 // // cross site request forgery helper
-// app.use(csrf())
+app.use(csrf())
 
 const MESSAGE = 'NADA' // placeholder message
 
-// app.use("*", async (c, next) => {
-//   const id = getCookie(c, lucia.sessionCookieName) ?? null
-//   if (!id) {
-//     c.set("user", null)
-//     c.set("session", null)
-//     return next()
-//   }
-//   const { session, user } = await lucia.validateSession(id)
-//   if (session && session.fresh) {
-//     c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), {
-//       append: true,
-//     })
-//   }
-//   if (!session) {
-//     c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), {
-//       append: true,
-//     })
-//   }
-//   c.set("user", user)
-//   c.set("session", session)
-//   return next()
-// })
+app.use("*", async (c, next) => {
+  const id = getCookie(c, lucia.sessionCookieName) ?? null
+  if (!id) {
+    c.set("user", null)
+    c.set("session", null)
+    return next()
+  }
+  const { session, user } = await lucia.validateSession(id)
+  if (session && session.fresh) {
+    c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), {
+      append: true,
+    })
+  }
+  if (!session) {
+    c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), {
+      append: true,
+    })
+  }
+  c.set("user", user)
+  c.set("session", session)
+  return next()
+})
 
 app.get('/', async (c) => {
   const user = c.get('user')

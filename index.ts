da5ef4c50646266db869b308c760e771c0b16d8d
jawndiego <jawndiego@gmail.com>
2024-06-13 13:15:41 -0400

    insert prev working code

--- index.ts
+++ index.ts
@@ -1,3 +1,364 @@
+// import { ed25519 } from '@noble/curves/ed25519'
+// import { blake3 } from '@noble/hashes/blake3'
+// import { randomBytes } from '@noble/hashes/utils'
+// import { origin } from 'bun'
+// import { getCookie } from 'hono/cookie'
+// import { csrf } from 'hono/csrf'
+// import { verifyRequestOrigin } from 'lucia'
+// import { generateRandomInteger, generateRandomString } from 'oslo/crypto'
+// import { kms } from './aws'
+// import { app } from './hono'
+// import { KEY_REF, publicKey } from './keys'
+// import { lucia } from './sessions'
+// import { signMessage, signMessageWithKey, verifyMessage } from './signatures'
+// import { writeClient } from './watcher'
+
+// // verifyRequestOrigin(origin, ["https://www.river.ph/*"])
+
+// // // cross site request forgery helper
+// // app.use(csrf())
+// // console.log("after app.use")
+
+// const MESSAGE = 'NADA' // placeholder message
+
+// // app.use("*", async (c, next) => {
+// //   const id = getCookie(c, lucia.sessionCookieName) ?? null
+// //   if (!id) {
+// //     c.set("user", null)
+// //     c.set("session", null)
+// //     return next()
+// //   }
+// //   const { session, user } = await lucia.validateSession(id)
+// //   if (session && session.fresh) {
+// //     c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize(), {
+// //       append: true,
+// //     })
+// //   }
+// //   if (!session) {
+// //     c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize(), {
+// //       append: true,
+// //     })
+// //   }
+// //   c.set("user", user)
+// //   console.log({user})
+// //   c.set("session", session)
+// //   console.log({session})
+// //   return next()
+// // })
+
+// // console.log("post app * * *")
+
+// // app.get('/', async (c) => {
+// //   const user = c.get('user')
+// //   console.log({inget: user})
+// //   if (!user) {
+// //     return c.body(null, 401)
+// //   }
+// // })
+
+// // console.log("post ////")
+
+// type SignatureResponse = { sig: string, signer: string }
+
+// function isSignatureResponse(data: any): data is SignatureResponse {
+//   return (
+//     typeof data === 'object' &&
+//     data !== null &&
+//     typeof data.sig === 'string' &&
+//     typeof data.signer === 'string'
+//   )
+// }
+
+// app.post('/signMessage', async (c) => {
+//   try {
+//     const { message } = await c.req.json()
+
+//     if (!message) {
+//       return c.json({ success: false, message: 'No message provided' }, 400)
+//     }
+
+//     const signedMessage = signMessage(message)
+
+//     return c.json({
+//       success: true,
+//       message,
+//       signedMessage,
+//     })
+//   } catch (error: unknown) {
+//     let errorMessage = 'An unknown error occurred'
+//     if (error instanceof Error) {
+//       errorMessage = error.message
+//     }
+//     return c.json({ success: false, message: errorMessage }, 500)
+//   }
+// })
+
+// app.post('/generateEncryptKeysAndSessionId', async (c) => {
+//   console.log('IN ENCRYPT ROUTE')
+//   try {
+//     const { message, signedMessage, deviceId } = await c.req.json()
+
+//     if (!message || !signedMessage || !deviceId) {
+//       return c.json({ success: false, message: 'Missing parameters' }, 400)
+//     }
+
+//     // Verify the signed message
+//     const isValid = verifyMessage(
+//       message,
+//       signedMessage,
+//       Buffer.from(publicKey).toString('hex'),
+//     )
+//     if (!isValid) {
+//       return c.json({ success: false, message: 'Invalid signature' }, 400)
+//     }
+
+//     console.log({ isValid })
+
+//     const publicKeyHex = Buffer.from(publicKey).toString('hex')
+
+//     // Check if a custody address exists in the hashes table
+//     const selectHashQuery = `
+//       SELECT userid FROM public.hashes
+//       WHERE custodyAddress = $1
+//     `
+//     const hashResult = await writeClient.query(selectHashQuery, [publicKeyHex])
+
+//     console.log({ hashResult })
+//     const userId = generateRandomInteger(100)
+//     let sessionId
+
+//     if (hashResult.rows.length === 0) {
+//       console.log('first time user!')
+//       // First-time user - generate keys, encrypt, and store them
+//       const eddsaPrivateKey = ed25519.utils.randomPrivateKey()
+//       const eddsaPublicKey = ed25519.getPublicKey(eddsaPrivateKey)
+
+//       const encryptedPrivateKey = await kms
+//         .encrypt({
+//           KeyId: KEY_REF,
+//           Plaintext: Buffer.from(eddsaPrivateKey),
+//         })
+//         .promise()
+
+//       const encryptedPublicKey = await kms
+//         .encrypt({
+//           KeyId: KEY_REF,
+//           Plaintext: Buffer.from(eddsaPublicKey),
+//         })
+//         .promise()
+
+//       if (
+//         !encryptedPrivateKey.CiphertextBlob ||
+//         !encryptedPublicKey.CiphertextBlob
+//       ) {
+//         throw new Error('Encryption failed')
+//       }
+
+//       // Store the encrypted keys
+//       console.log('prestorekeys')
+//       const insertKeysQuery = `
+//         INSERT INTO public.hashes (userid, custodyAddress, deviceid, encryptedprivatekey, encryptedpublickey)
+//         VALUES ($1, $2, $3, $4, $5)
+//       `
+
+//       await writeClient.query(insertKeysQuery, [
+//         userId,
+//         publicKeyHex,
+//         deviceId,
+//         encryptedPrivateKey.CiphertextBlob.toString('base64'),
+//         encryptedPublicKey.CiphertextBlob.toString('base64'),
+//       ])
+
+//       const expiresAt = new Date(Date.now() + 2 * 7 * 24 * 60 * 60 * 1000)
+
+//       // Create a session with Lucia
+//       const session = await lucia.createSession(userId.toString(), { userId: userId.toString(), expiresAt, deviceId })
+//       sessionId = session.id
+//     } else {
+//       console.log('returning user!')
+//       // Returning user - update session
+//       const userId = hashResult.rows[0].userid
+
+//       const expiresAt = new Date(Date.now() + 2 * 7 * 24 * 60 * 60 * 1000)
+//       const session = await lucia.createSession(userId.toString(), { userId: userId.toString(), expiresAt, deviceId })
+//       sessionId = session.id
+
+//       console.log({ userId, sessionId })
+//     }
+
+//     // Create session cookie
+//     const sessionCookie = lucia.createSessionCookie(sessionId)
+//     console.log({sessionCookie})
+
+//     c.header("Set-Cookie", sessionCookie.serialize(), { append: true })
+
+//     return c.json({
+//       success: true,
+//       userId,
+//       sessionId,
+//     })
+//   } catch (error: unknown) {
+//     let errorMessage = 'An unknown error occurred'
+//     if (error instanceof Error) {
+//       errorMessage = error.message
+//     }
+//     return c.json({ success: false, message: errorMessage }, 500)
+//   }
+// })
+
+
+
+// app.post('/signMessageWithSession', async (c) => {
+//   try {
+//     const { sessionId, message } = await c.req.json()
+
+//     if (!sessionId || !message) {
+//       return c.json({ success: false, message: 'Missing parameters' }, 400)
+//     }
+
+//     // Retrieve the session and associated user
+//     const selectSessionQuery = `
+//       SELECT s.userid, h.custodyAddress FROM public.sessions s
+//       JOIN public.hashes h ON s.userid = h.userid
+//       WHERE s.id = $1
+//     `
+//     const sessionResult = await writeClient.query(selectSessionQuery, [
+//       sessionId,
+//     ])
+//     console.log({ sessionResult })
+
+//     if (sessionResult.rows.length === 0) {
+//       return c.json({ success: false, message: 'Invalid session' }, 404)
+//     }
+
+//     const { userid } = sessionResult.rows[0]
+
+//     // Retrieve the stored encrypted keys
+//     const selectKeysQuery = `
+//       SELECT encryptedprivatekey FROM public.hashes
+//       WHERE userid = $1
+//     `
+//     const keysResult = await writeClient.query(selectKeysQuery, [userid])
+
+//     if (keysResult.rows.length === 0) {
+//       return c.json({ success: false, message: 'Keys not found' }, 404)
+//     }
+
+//     const { encryptedprivatekey } = keysResult.rows[0]
+
+//     // Decrypt the private key using AWS KMS
+//     const decryptedPrivateKey = await kms
+//       .decrypt({
+//         CiphertextBlob: Buffer.from(encryptedprivatekey, 'base64'),
+//       })
+//       .promise()
+
+//     if (!decryptedPrivateKey.Plaintext) {
+//       throw new Error('Decryption failed')
+//     }
+
+//     // Sign the message with the decrypted EDDSA private key
+//     const eddsaPrivateKey = new Uint8Array(
+//       decryptedPrivateKey.Plaintext as ArrayBuffer,
+//     )
+//     const signedMessage = signMessageWithKey(message, eddsaPrivateKey)
+
+//     // Re-encrypt the private key using AWS KMS
+//     const reEncryptedPrivateKey = await kms
+//       .encrypt({
+//         KeyId: KEY_REF,
+//         Plaintext: Buffer.from(eddsaPrivateKey),
+//       })
+//       .promise()
+
+//     if (!reEncryptedPrivateKey.CiphertextBlob) {
+//       throw new Error('Re-encryption failed')
+//     }
+
+//     const updateKeysQuery = `
+//     UPDATE public.hashes
+//     SET encryptedprivatekey = $1
+//     WHERE userid = $2
+//   `
+//   await writeClient.query(updateKeysQuery, [
+//     reEncryptedPrivateKey.CiphertextBlob.toString('base64'),
+//     userid,
+//   ])
+
+//     return c.json({
+//       success: true,
+//       signedMessage,
+//     })
+//   } catch (error: unknown) {
+//     let errorMessage = 'An unknown error occurred'
+//     if (error instanceof Error) {
+//       errorMessage = error.message
+//     }
+//     return c.json({ success: false, message: errorMessage }, 500)
+//   }
+// })
+
+// app.get('/submitToChannel', async (c) => {
+//   try {
+//     // Request a signature from the KMS VM
+//     const response = await fetch(
+//       `https://240608-server-studies-production.up.railway.app/signMessage`,
+//       {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({ message: MESSAGE }),
+//       },
+//     )
+
+//     if (!response.ok) {
+//       throw new Error('Failed to get signature from KMS')
+//     }
+
+//     const data: unknown = await response.json()
+
+//     if (!isSignatureResponse(data)) {
+//       throw new Error('KMS return invalid')
+//     }
+
+//     const { sig, signer } = data
+//     const isValid = verifyMessage(MESSAGE, sig, signer)
+
+//     if (isValid) {
+//       // Generate a BLAKE3 hash for submissionId
+//       const submissionId = blake3(new TextEncoder().encode(MESSAGE))
+
+//       // Insert the new row into the submissions table
+//       const insertQuery = `
+//        INSERT INTO public.submissions (submissionId, submissionContents, submissionSig, submissionSigner)
+//        VALUES ($1, $2, $3, $4)
+//      `
+//       await writeClient.query(insertQuery, [submissionId, MESSAGE, sig, signer])
+
+//       return c.json({
+//         success: true,
+//         message: 'Signature verified successfully',
+//         body: { submissionId, MESSAGE, sig, signer },
+//       })
+//     } else {
+//       return c.json({ success: false, message: 'Failed to verify signature' })
+//     }
+//   } catch (error) {
+//     return c.json({ success: false, message: error })
+//   }
+// })
+
+// Bun.serve({
+//   fetch: app.fetch,
+//   port: process.env.PORT || 3030,
+// })
+
+// console.log("served")
+
+// console.log(
+//   `Hono server started on http://localhost:${process.env.PORT || 3030}`,
+// )
+
+
 import { ed25519 } from '@noble/curves/ed25519'
 import { blake3 } from '@noble/hashes/blake3'
 import { randomBytes } from '@noble/hashes/utils'
@@ -17,7 +378,6 @@ import { writeClient } from './watcher'
 
 // // cross site request forgery helper
 // app.use(csrf())
-// console.log("after app.use")
 
 const MESSAGE = 'NADA' // placeholder message
 
@@ -40,25 +400,18 @@ const MESSAGE = 'NADA' // placeholder message
 //     })
 //   }
 //   c.set("user", user)
-//   console.log({user})
 //   c.set("session", session)
-//   console.log({session})
 //   return next()
 // })
 
-// console.log("post app * * *")
+app.get('/', async (c) => {
+  const user = c.get('user')
+  if (!user) {
+    return c.body(null, 401)
+  }
+})
 
-// app.get('/', async (c) => {
-//   const user = c.get('user')
-//   console.log({inget: user})
-//   if (!user) {
-//     return c.body(null, 401)
-//   }
-// })
-
-// console.log("post ////")
-
-type SignatureResponse = { sig: string, signer: string }
+type SignatureResponse = { sig: string; signer: string }
 
 function isSignatureResponse(data: any): data is SignatureResponse {
   return (
@@ -154,7 +507,40 @@ app.post('/generateEncryptKeysAndSessionId', async (c) => {
         throw new Error('Encryption failed')
       }
 
+      // const insertUserQuery = `
+      //   INSERT INTO public.users (recovery, "to", log_addr, block_num)
+      //   VALUES ($1, $2, $3, $4)
+      //   RETURNING userid
+      // `
+
+      // const newUserResult = await writeClient.query(insertUserQuery, [
+      //   publicKeyHex,
+      //   null,
+      //   null,
+      //   0,
+      // ])
+
+      // console.log({newUserResult})
+
+      // // this is not real lol
+      // userId = newUserResult.rows[0].id
+
+      const insertSessionQuery = `
+        INSERT INTO public.sessions (userid, id, expiration, deviceid)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id
+      `
+      const newSessionResult = await writeClient.query(insertSessionQuery, [
+        userId,
+        generateRandomString(10, 'a-z'),
+        new Date(Date.now() + 2 * 7 * 24 * 60 * 60 * 1000), // 2 weeks from now
+        deviceId,
+      ])
+
+      sessionId = newSessionResult.rows[0].id
+
       // Store the encrypted keys
+
       console.log('prestorekeys')
       const insertKeysQuery = `
         INSERT INTO public.hashes (userid, custodyAddress, deviceid, encryptedprivatekey, encryptedpublickey)
@@ -168,29 +554,27 @@ app.post('/generateEncryptKeysAndSessionId', async (c) => {
         encryptedPrivateKey.CiphertextBlob.toString('base64'),
         encryptedPublicKey.CiphertextBlob.toString('base64'),
       ])
-
-      const expiresAt = new Date(Date.now() + 2 * 7 * 24 * 60 * 60 * 1000)
-
-      // Create a session with Lucia
-      const session = await lucia.createSession(userId.toString(), { userId: userId.toString(), expiresAt, deviceId })
-      sessionId = session.id
     } else {
-      console.log('returning user!')
-      // Returning user - update session
-      const userId = hashResult.rows[0].userid
+      // Returning user - update session ID
+      // userId = hashResult.rows[0].userid
 
-      const expiresAt = new Date(Date.now() + 2 * 7 * 24 * 60 * 60 * 1000)
-      const session = await lucia.createSession(userId.toString(), { userId: userId.toString(), expiresAt, deviceId })
-      sessionId = session.id
+      console.log({ userId })
 
-      console.log({ userId, sessionId })
+      const updateSessionQuery = `
+        UPDATE public.sessions
+        SET id = $1, expiration = $2, deviceid = $3
+        WHERE userid = $4
+        RETURNING id
+      `
+      const updatedSessionResult = await writeClient.query(updateSessionQuery, [
+        'updatedSessionData',
+        new Date(Date.now() + 2 * 7 * 24 * 60 * 60 * 1000),
+        deviceId,
+        userId,
+      ])
+      sessionId = updatedSessionResult.rows[0].id
+      console.log(updatedSessionResult)
     }
-
-    // Create session cookie
-    const sessionCookie = lucia.createSessionCookie(sessionId)
-    console.log({sessionCookie})
-
-    c.header("Set-Cookie", sessionCookie.serialize(), { append: true })
 
     return c.json({
       success: true,
@@ -205,8 +589,6 @@ app.post('/generateEncryptKeysAndSessionId', async (c) => {
     return c.json({ success: false, message: errorMessage }, 500)
   }
 })
-
-
 
 app.post('/signMessageWithSession', async (c) => {
   try {
@@ -352,9 +734,6 @@ Bun.serve({
   port: process.env.PORT || 3030,
 })
 
-console.log("served")
-
 console.log(
   `Hono server started on http://localhost:${process.env.PORT || 3030}`,
 )
-
--- watcher.ts
+++ watcher.ts
@@ -1,9 +1,167 @@
+// import pg from 'pg'
+// import { app } from './hono'
+
+// const { Client } = pg
+
+// console.log("YO WATCHER")
+
+// const listenConnectionString = process.env.LISTEN_DATABASE_URL!
+// const writeConnectionString = process.env.WRITE_DATABASE_URL!
+
+// export const listenClient = new Client({
+//   connectionString: listenConnectionString,
+// })
+
+// export const writeClient = new Client({
+//   connectionString: writeConnectionString,
+// })
+
+// listenClient
+//   .connect()
+//   .then(() => console.log('Connected to Source DB successfully'))
+//   .catch((err) =>
+//     console.error('Connection error with Source DB:', (err as Error).stack),
+//   )
+
+//   console.log({listenClient})
+
+// writeClient
+//   .connect()
+//   .then(() => {
+//     console.log('Connected to Destination DB successfully')
+//     ensureTablesExist()
+//   })
+//   .catch((err) =>
+//     console.error(
+//       'Connection error with Destination DB:',
+//       (err as Error).stack,
+//     ),
+//   )
+
+//   console.log({writeClient})
+
+// async function ensureTablesExist() {
+//   try {
+//     await writeClient.query('BEGIN')
+
+//     // Create users table
+//     await writeClient.query(`
+//       CREATE TABLE IF NOT EXISTS public.users (
+//         userid SERIAL PRIMARY KEY,
+//         "to" TEXT,
+//         recovery TEXT,
+//         timestamp TIMESTAMP,
+//         log_addr TEXT,
+//         block_num NUMERIC
+//       )
+//     `)
+
+//     // Create sessions table
+//     await writeClient.query(`
+//       CREATE TABLE IF NOT EXISTS public.sessions (
+//         id TEXT PRIMARY KEY,
+//         userid TEXT NOT NULL REFERENCES public.users(userid),
+//         deviceid TEXT NOT NULL,
+//         created TIMESTAMP,
+//         expiresAt TIMESTAMP NOT NULL
+//       )
+//     `)
+
+//     // Create hashes table
+//     await writeClient.query(`
+//       CREATE TABLE IF NOT EXISTS public.hashes (
+//         userid TEXT NOT NULL REFERENCES public.users(userid),
+//         custodyAddress TEXT NOT NULL,
+//         deviceid TEXT NOT NULL,
+//         encryptedpublickey TEXT NOT NULL,
+//         encryptedprivatekey TEXT NOT NULL,
+//         PRIMARY KEY (userid, custodyAddress, deviceid)
+//       )
+//     `)
+
+//     await writeClient.query('COMMIT')
+//     console.log('Schema and tables verified/created successfully')
+//   } catch (err) {
+//     await writeClient.query('ROLLBACK')
+//     console.error(
+//       'Error in schema/table creation in destination DB:',
+//       (err as Error).stack,
+//     )
+//   }
+// }
+
+// async function checkAndReplicateData() {
+//   try {
+//     const maxBlockQueryResult = await writeClient.query(`
+//       SELECT COALESCE(MAX(block_num), 0) as max_block_number FROM public.users
+//     `)
+//     const lastProcessedBlockNumber =
+//       maxBlockQueryResult.rows[0].max_block_number
+
+//     const queryResult = await listenClient.query(
+//       `
+//       SELECT userid, "to", recovery, timestamp, log_addr, block_num FROM users
+//       WHERE block_num > $1
+//       ORDER BY block_num ASC
+//     `,
+//       [lastProcessedBlockNumber],
+//     )
+
+//     if (queryResult.rows.length > 0) {
+//       const res = await writeClient.query(
+//         `
+//   INSERT INTO public.users (userid, "to", recovery, timestamp, log_addr, block_num)
+//   SELECT * FROM unnest($1::NUMERIC[], $2::TEXT[], $3::TEXT[], $4::TIMESTAMP[], $5::TEXT[], $6::NUMERIC[])
+//   ON CONFLICT (userid) DO UPDATE
+//   SET "to" = EXCLUDED."to", recovery = EXCLUDED.recovery, timestamp = EXCLUDED.timestamp, log_addr = EXCLUDED.log_addr, block_num = EXCLUDED.block_num
+//   RETURNING *
+// `,
+//         [
+//           queryResult.rows.map((row) => row.userid),
+//           queryResult.rows.map((row) => row.to),
+//           queryResult.rows.map((row) => row.recovery),
+//           queryResult.rows.map((row) => row.timestamp),
+//           queryResult.rows.map((row) => row.log_addr),
+//           queryResult.rows.map((row) => row.block_num),
+//         ],
+//       )
+//       console.log('Data replicated:', res.rows)
+//     }
+//   } catch (err) {
+//     console.error('Error during data replication', (err as Error).stack)
+//   }
+// }
+
+// setInterval(checkAndReplicateData, 1000)
+
+// process.on('SIGINT', () => {
+//   console.log("yo sigint")
+//   Promise.all([listenClient.end(), writeClient.end()])
+//     .then(() => {
+//       console.log('Both clients disconnected')
+//       process.exit()
+//     })
+//     .catch((err) =>
+//       console.error('Error during disconnection', (err as Error).stack),
+//     )
+// })
+
+// Bun.serve({
+//   fetch: app.fetch,
+//   port: process.env.PORT || 3030,
+// })
+
+// console.log("served")
+
+// console.log(
+//   `Hono server started on http://localhost:${process.env.PORT || 3030}`,
+// )
+
+
 import pg from 'pg'
 import { app } from './hono'
 
 const { Client } = pg
-
-console.log("YO WATCHER")
 
 const listenConnectionString = process.env.LISTEN_DATABASE_URL!
 const writeConnectionString = process.env.WRITE_DATABASE_URL!
@@ -23,8 +181,6 @@ listenClient
     console.error('Connection error with Source DB:', (err as Error).stack),
   )
 
-  console.log({listenClient})
-
 writeClient
   .connect()
   .then(() => {
@@ -37,8 +193,6 @@ writeClient
       (err as Error).stack,
     ),
   )
-
-  console.log({writeClient})
 
 async function ensureTablesExist() {
   try {
@@ -135,7 +289,6 @@ async function checkAndReplicateData() {
 setInterval(checkAndReplicateData, 1000)
 
 process.on('SIGINT', () => {
-  console.log("yo sigint")
   Promise.all([listenClient.end(), writeClient.end()])
     .then(() => {
       console.log('Both clients disconnected')
@@ -146,14 +299,13 @@ process.on('SIGINT', () => {
     )
 })
 
+app.get('/', (c) => c.text('Hello, Hono!'))
+
 Bun.serve({
   fetch: app.fetch,
   port: process.env.PORT || 3030,
 })
 
-console.log("served")
-
 console.log(
   `Hono server started on http://localhost:${process.env.PORT || 3030}`,
 )
-

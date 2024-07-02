17b82c167a01048054931d9c9f974f86e3c0d515
jawndiego <jawndiego@gmail.com>
2024-07-02 12:09:07 -0400

    momentary middleware removal

--- server.ts
+++ server.ts
@@ -29,36 +29,36 @@ app.use(
 )
 
 // Session and user handling middleware
-app.use('*', async (c, next) => {
-  const sessionId = getCookie(c, lucia.sessionCookieName) ?? null
-  console.log('Session ID:', sessionId)
+// app.use('*', async (c, next) => {
+//   const sessionId = getCookie(c, lucia.sessionCookieName) ?? null
+//   console.log('Session ID:', sessionId)
 
-  if (!sessionId) {
-    c.set('user', null)
-    c.set('session', null)
-    console.log('No session ID found, setting user and session to null')
-    return next()
-  }
+//   if (!sessionId) {
+//     c.set('user', null)
+//     c.set('session', null)
+//     console.log('No session ID found, setting user and session to null')
+//     return next()
+//   }
 
-  const { session, user } = await lucia.validateSession(sessionId)
+//   const { session, user } = await lucia.validateSession(sessionId)
 
-  console.log('Validated Session:', session)
-  if (session && session.fresh) {
-    c.header('Set-Cookie', lucia.createSessionCookie(session.id).serialize(), {
-      append: true,
-    })
-    console.log('Session is fresh, setting session cookie')
-  } else if (!session) {
-    c.header('Set-Cookie', lucia.createBlankSessionCookie().serialize(), {
-      append: true,
-    })
-    console.log('No valid session found, setting blank session cookie')
-  }
+//   console.log('Validated Session:', session)
+//   if (session && session.fresh) {
+//     c.header('Set-Cookie', lucia.createSessionCookie(session.id).serialize(), {
+//       append: true,
+//     })
+//     console.log('Session is fresh, setting session cookie')
+//   } else if (!session) {
+//     c.header('Set-Cookie', lucia.createBlankSessionCookie().serialize(), {
+//       append: true,
+//     })
+//     console.log('No valid session found, setting blank session cookie')
+//   }
 
-  c.set('user', user)
-  c.set('session', session)
-  return next()
-})
+//   c.set('user', user)
+//   c.set('session', session)
+//   return next()
+// })
 
 // Example route (uncomment and customize as needed)
 // app.get('/', async (c) => {
